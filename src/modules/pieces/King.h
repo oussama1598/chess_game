8563d4956a19daf04110577f1603b7bb0ffef76f
Oussama Barkouki <oussama1598@gmail.com>
2020-04-21 18:04:29 +0000

    Finished all movements for the king

--- src/modules/pieces/King.h
+++ src/modules/pieces/King.h
@@ -7,4 +7,7 @@ class King : public Piece
 {
 public:
     explicit King(int player_id);
+
+    std::vector<std::string>
+    get_possible_moves(bool is_top, const std::string &from) override;
 };
--- test/pieces/king.cpp
+++ test/pieces/king.cpp
@@ -3,12 +3,28 @@
 namespace {
     TEST_CASE("King Movements Tests") {
         Player top_player{0, true, true, false};
-        Player down_player{0, true, false, false};
         King king(top_player.player_id);
 
+        std::map<std::string, std::vector<std::string>> testing_cases{
+                {"F5", {"E6", "F6", "G6", "E5", "G5", "E4", "F4", "G4"}},
+                {"A1", {"A2", "B2", "B1"}},
+                {"A8", {"B8", "B7", "A7"}},
+                {"H8", {"G8", "G7", "H7"}},
+                {"H1", {"G2", "G1", "H2"}}
+        };
+
+
+        for (auto &test_case: testing_cases)
+            SECTION("Test all the possible moves from " + test_case.first) {
+                std::string from{test_case.first};
+
+                CHECK_THAT(king.get_possible_moves(false, from),
+                           Catch::UnorderedEquals(test_case.second)
+                );
+            }
 
         SECTION("Test the is valid move") {
-            CHECK(!king.is_valid_move(down_player, "A1", "A2"));
+            CHECK(king.is_valid_move(top_player, "F5", "E6"));
         }
     }
 }
